14b2b9831855e5f1ba5f5de8bead61ad66cedc41
lnmangione <lnmangione@gmail.com>
2020-05-26 13:51:09 -0400

    Implemented merge sort, quick sort

--- eg_sort.py
+++ eg_sort.py
@@ -4,7 +4,7 @@
 Like a hand of cards: maintains sorted left sub-array and repeatedly inserts into it
 O(n^2), in-place, stable
 """
-def insertion_sort(l: list) -> list:
+def insertion_sort(l: list) -> None:
     for i in range(1, len(l)):
         # Repeatedly shift elements rightward
         key = l[i]
@@ -14,15 +14,14 @@ def insertion_sort(l: list) -> list:
             j -= 1
         # Place element i
         l[j + 1] = key
-    return l
 
 
 """
 Maintains sorted left sub-array, and repeatedly selects the minimum element from the unsorted
 sub-array to place at the end of the left sub-array
-O(n^2)
+O(n^2), in-place, not stable
 """
-def selection_sort(l: list) -> list:
+def selection_sort(l: list) -> None:
     for i in range(len(l) - 1):
         # Find minimum element index in remaining sub-array
         min_i = i
@@ -31,13 +30,84 @@ def selection_sort(l: list) -> list:
                 min_i = j
         # Swap min element with start of unsorted sub-array
         l[i], l[min_i] = l[min_i], l[i]
-    return l
 
+"""
+Merge two sorted lists in O(n) time. Requires O(n) extra space
+"""
+def merge(a: list, b: list) -> list:
+    a_i, b_i, c = 0, 0, []
+
+    while a_i < len(a) and b_i < len(b):
+        if a[a_i] <= b[b_i]:  # using '<=' here makes mergesort stable
+            c.append(a[a_i])
+            a_i += 1
+        else:
+            c.append(b[b_i])
+            b_i += 1
+
+    if a_i < len(a):
+        c.extend(a[a_i:])
+    elif b_i < len(b):
+        c.extend(b[b_i:])
+
+    return c
+
+"""
+Recursively sorts left, right sub-arrays and then merges with
+linear merge sub-routine
+O(n*log(n)), not in-place, stable
+"""
+def merge_sort(l: list) -> list:
+    if len(l) <= 1:
+        return l
+    mid = len(l) // 2
+    return merge(merge_sort(l[:mid]), merge_sort(l[mid:]))
+
+
+"""
+Rearrange arr[lo...hi] in-place in linear time such that all elements
+less than pivot are left of it, and all elements greater than or equal to pivot
+are right of it.
+Return pivot index
+lo and hi are inclusive indices within which to partition
+"""
+def partition(arr: list, lo: int, hi: int) -> int:
+    pivot = arr[hi]  # arbitrarily choosing last element as pivot
+    pivot_idx = hi
+    hi -= 1
+    # maintain arr[left of lo] < pivot, arr[right of hi] >= pivot
+    while lo <= hi:
+        if arr[lo] < pivot:
+            lo += 1
+        else:
+            arr[lo], arr[hi] = arr[hi], arr[lo]
+            hi -= 1
+
+    arr[lo], arr[pivot_idx] = arr[pivot_idx], arr[lo]
+    return lo
+
+
+"""
+Partitions array, then recursively sorts left and right sub-arrays
+Expected O(n*log(n)), in-place, not stable
+"""
+def quick_sort(arr: list, lo=None, hi=None) -> None:
+    if lo is None:
+        lo, hi = 0, len(arr) - 1
+    if hi <= lo:
+        return
+    pivot_idx = partition(arr, lo, hi)
+    quick_sort(arr, lo, pivot_idx - 1)
+    quick_sort(arr, pivot_idx + 1, hi)
 
 
 
 
 
 l = [4, 3, 2, 7, 8, 1, 5, 6]
-# print(insertion_sort(l))
-print(selection_sort(l))
+# insertion_sort(l)
+# selection_sort(l)
+# print(merge_sort(l))
+
+quick_sort(l)
+print(l)
